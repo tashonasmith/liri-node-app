f42d96764de189049537b6551b0a58eb9914fa41
Tashona Smith <tashona.smith@gmail.com>
2020-05-03 22:48:23 -0500

    Complete do-what-it-says command

--- liri.js
+++ liri.js
@@ -17,6 +17,8 @@ var moment = require('moment');
 
 var axios = require("axios");
 
+var fs = require('file-system')
+
 dataArr = process.argv.slice(3);
 
 if (process.argv[2] === "concert-this") {
@@ -102,7 +104,130 @@ if (process.argv[2] === "concert-this") {
             }
             console.log(error.config);
         });       
-}
+ } 
+// else if (process.argv[2] === "movie-this" && dataArr === null) {
+//     dataArr.push("Mr.", "Nobody");
+//     var movie = dataArr.join(" ");
+//     var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+//     axios.get(queryUrl).then(function(response) {
+//         console.log(queryUrl);
+//     })
+    // .catch(function(error) {
+    //     if (error.response) {
+    //       // The request was made and the server responded with a status code
+    //       // that falls out of the range of 2xx
+    //       console.log("---------------Data---------------");
+    //       console.log(error.response.data);
+    //       console.log("---------------Status---------------");
+    //       console.log(error.response.status);
+    //       console.log("---------------Status---------------");
+    //       console.log(error.response.headers);
+    //     } else if (error.request) {
+    //       // The request was made but no response was received
+    //       // `error.request` is an object that comes back with details pertaining to the error that occurred.
+    //       console.log(error.request);
+    //     } else {
+    //       // Something happened in setting up the request that triggered an Error
+    //       console.log("Error", error.message);
+    //     }
+    //     console.log(error.config);
+    // });       
+//}
+else if (process.argv[2] === "do-what-it-says") {
+fs.readFile("random.txt", "utf8", function(error, data) {
+
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+
+    var randomArr = data.split(",");
+    
+    console.log(randomArr);
+    console.log(randomArr[0]);
+    console.log(randomArr[1]);
+    
+    if (randomArr[0] === "concert-this") {
+        var artist = randomArr[1];
+        queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            //console.log(response);
+            for (i = 0; i < response.length; i++) {
+                console.log("Artist: " + artist);
+                console.log("Name of Venue: " + response[i].venue.name);
+                console.log("Venue Location: " + response[i].venue.city);
+                var eventDate = moment(response[i].datetime).format("MM/DD/YYYY");
+                console.log("Date of the Event: " + eventDate);
+                console.log("---------------------------------------------------");
+                
+            };
+        });
+    
+    } else if (randomArr[0] === "spotify-this-song") {
+      var track = randomArr[1];
+      spotify.search({ type: 'track', query: track, limit: 1 }, function(err, data) {
+          if (err) {
+            return console.log('Error occurred: ' + err);
+          } 
+      
+        //console.log(data.tracks.items[0]);  
+        console.log("--------------------------------------------------")
+        console.log("Artist(s): " + data.tracks.items[0].album.artists[0].name); 
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("--------------------------------------------------")
+      });
+
+    } else if (randomArr[0] === "movie-this") {
+        var movie = randomArr[1]
+        var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    
+        axios.get(queryUrl).then(
+            function(response) {
+              //console.log(response.data);
+              console.log("----------------------------------------------------------");
+              console.log("Title: " + response.data.Title);
+              console.log("Year Released: " + response.data.Year);
+              console.log("IMDB Rating: " + response.data.imdbRating);
+              console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+              console.log("Produced In: " + response.data.Country);
+              console.log("Language: " + response.data.Language);
+              console.log("Plot: " + response.data.Plot);
+              console.log("Starring: " + response.data.Actors);
+              console.log("-----------------------------------------------------------");
+            })
+            .catch(function(error) {
+                if (error.response) {
+                  // The request was made and the server responded with a status code
+                  // that falls out of the range of 2xx
+                  console.log("---------------Data---------------");
+                  console.log(error.response.data);
+                  console.log("---------------Status---------------");
+                  console.log(error.response.status);
+                  console.log("---------------Status---------------");
+                  console.log(error.response.headers);
+                } else if (error.request) {
+                  // The request was made but no response was received
+                  // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                  console.log(error.request);
+                } else {
+                  // Something happened in setting up the request that triggered an Error
+                  console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
+
+  
+  };
+});
+};
+
 
 
 
